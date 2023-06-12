62d1d46eade3ec981b6dbd129a53c853185eac05
hssudusinghe <135609482+hssudusinghe@users.noreply.github.com>
2023-06-12 04:45:05 -0700

    Add files via upload

---
+++ clab.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+int main()
+{
+    /*int no;
+    printf ("enter a number");
+    scanf("%d",&no);
+
+    if(no % 2 ==1){
+        printf("%d is an odd number\n",no);
+    }
+    else{
+        printf("%d is an even number\n",no);
+    }*/
+
+
+   /* int no;
+    printf("enter an integer:");
+    scanf("%d",&no);
+    switch(no%2){
+    case 0:
+    printf("%d is even number.\n",no);
+    break;
+    case 1:
+    printf("%d is odd number.\n,",no);
+    break;
+    }*/
+
+
+       /* int num1,num2,choice;
+        //display the menu
+        printf("menu:\n");
+        printf("1.addition\n");
+        printf("2.substraction\n");
+        printf("3.multiplicatipn\n");
+        printf("4.division\n");
+        printf("Enter your choice\n");
+        scanf("%d",&choice);
+        printf("Enter two numbers:");
+        scanf ("%d %d",&num1,&num2);
+        switch(choice){
+        case 1:
+            printf ("result: %d\n",num1+num2);
+            break;
+
+        case 2:
+            printf("result: %d\n",num1-num2);
+        break;
+        case 3:
+            printf("result; %d\n",num1*num2);
+        break;
+        case 4:
+            printf("result; %d\n",num1/num2);
+        break;
+        default:
+        printf("invalied choice.\n");}
+        return 0;*/
+
+
+
+
+
+
